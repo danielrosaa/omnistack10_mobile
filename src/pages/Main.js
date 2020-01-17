0cfb8edee7e26349c3b2819d9745f57e26618770
Daniel <drosa@indracompany.com>
2020-01-17 17:26:18 -0300

    Configuração de websocket.

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,6 +5,7 @@ import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons'
 
 import api from './../services/api'
+import { connect, disconnect, subscribeToNewDevs } from './../services/socket'
 
 function Main({ navigation }) {
     const [devs, setDevs] = useState([])
@@ -34,6 +35,22 @@ function Main({ navigation }) {
         loadInitialLocation()
     }, [])
 
+    useEffect(() => {
+        subscribeToNewDevs(dev => setDevs([...devs, dev]))
+    }, [devs])
+
+    function setupWebSocket() {
+        disconnect()
+
+        const { latitude, longitude } = currentRegion
+
+        connect(
+            latitude,
+            longitude,
+            techs
+        )
+    }
+
     async function loadDevs() {
         const { latitude, longitude } = currentRegion
         let techLowerCase = techs.toLowerCase()
@@ -44,7 +61,9 @@ function Main({ navigation }) {
                 techs: techLowerCase
             }
         })
+
         setDevs(response.data.devs)
+        setupWebSocket()
     }
 
     function handleRegionChange(region) {
